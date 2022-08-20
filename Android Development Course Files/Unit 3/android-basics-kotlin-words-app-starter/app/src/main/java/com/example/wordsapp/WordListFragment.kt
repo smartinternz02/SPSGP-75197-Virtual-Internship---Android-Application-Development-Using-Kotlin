bb4082461934ff6406a522bedaa9695c5d883420
namanarora09 <namanarora0908@gmail.com>
2022-08-20 19:10:03 +0530

    Android

--- Android Development Course Files/Unit 3/android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ Android Development Course Files/Unit 3/android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -12,10 +12,19 @@ import com.example.wordsapp.databinding.FragmentWordListBinding
 class WordListFragment : Fragment() {
     private var _binding:FragmentWordListBinding?=null
     private val binding get()=_binding!!
+    private lateinit var letterId: String
 
     companion object{
         const val LETTER="letter"
         const val SEARCH_PREFIX="https://www.google.com/search?q="
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
     }
 
     override fun onCreateView(
@@ -30,7 +39,7 @@ class WordListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(), requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
 
         recyclerView.addItemDecoration(
             DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
