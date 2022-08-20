8e59b12b9b6d7310a3bacc23179a603715e69381
namanarora09 <namanarora0908@gmail.com>
2022-08-20 18:23:39 +0530

    Android

--- Android Development Course Files/Unit 3/android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ Android Development Course Files/Unit 3/android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -11,10 +11,10 @@ import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 
 class LetterListFragment : Fragment() {
-    private var _binding:FragmentLetterListBinding?=null
+    private var _binding: FragmentLetterListBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
-    private var isLinearLayoutManager=true
+    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,66 +22,64 @@ class LetterListFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding=FragmentLetterListBinding.inflate(inflater,container,false)
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        recyclerView = binding.recyclerView
         chooseLayout()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding=null
+        _binding = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.layout_menu,menu)
+        inflater.inflate(R.menu.layout_menu, menu)
 
-        val layoutButton=menu.findItem(R.id.action_switch_layout)
+        val layoutButton = menu.findItem(R.id.action_switch_layout)
         setIcon(layoutButton)
     }
 
-    private fun chooseLayout(){
-        if(isLinearLayoutManager){
-            recyclerView.layoutManager= LinearLayoutManager(this)
-        }else{
-            recyclerView.layoutManager= GridLayoutManager(this,4)
+    private fun chooseLayout() {
+        when (isLinearLayoutManager) {
+            true -> {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = LetterAdapter()
+            }
+            false -> {
+                recyclerView.layoutManager = GridLayoutManager(context, 4)
+                recyclerView.adapter = LetterAdapter()
+            }
         }
-        recyclerView.adapter=LetterAdapter()
     }
 
-    private fun setIcon(menuItem: MenuItem?){
-        if(menuItem==null)
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null)
             return
-        menuItem.icon=
-            if(isLinearLayoutManager)
-                ContextCompat.getDrawable(this,R.drawable.ic_grid_layout)
-            else
-                ContextCompat.getDrawable(this,R.drawable.ic_linear_layout)
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
-            R.id.action_switch_layout->{
-                // Sets isLinearLayoutManager (a Boolean) to the opposite value
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
                 isLinearLayoutManager = !isLinearLayoutManager
-                // Sets layout and icon
                 chooseLayout()
                 setIcon(item)
 
                 return true
             }
-            //  Otherwise, do nothing and use the core event handling
 
-            // when clauses require that all possible paths be accounted for explicitly,
-            //  for instance both the true and false cases if the value is a Boolean,
-            //  or an else to catch all unhandled cases.
             else -> super.onOptionsItemSelected(item)
         }
     }
