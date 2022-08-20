0f1b61763fe780d1446657e02376922e28690f9e
namanarora09 <namanarora0908@gmail.com>
2022-08-20 18:56:28 +0530

    Android

--- Android Development Course Files/Unit 3/android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ Android Development Course Files/Unit 3/android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -38,7 +38,7 @@ class LetterAdapter :
      * Provides a reference for the views needed to display items in your list.
      */
     class LetterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val button = view.findViewById<Button>(R.id.button_item)
+        val button= view.findViewById<Button>(R.id.button_item)
     }
 
     override fun getItemCount(): Int {
@@ -65,7 +65,7 @@ class LetterAdapter :
         holder.button.text = item.toString()
         holder.button.setOnClickListener{
             val context=holder.view.context
-            val intent=Intent(context,DetailActivity::class.java)
+            val intent=Intent(context,WordListFragment::class.java)
             intent.putExtra(WordListFragment.LETTER,holder.button.text.toString())
             context.startActivity(intent)
         }
