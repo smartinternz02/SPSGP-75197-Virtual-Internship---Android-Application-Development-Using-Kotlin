86e5223afa1705c0e70fb991b332aae868257bb7
namanarora09 <namanarora0908@gmail.com>
2022-08-22 12:36:45 +0530

    Android

--- Android Development Course Files/Unit 3/android-basics-kotlin-lunch-tray-app-main/app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ Android Development Course Files/Unit 3/android-basics-kotlin-lunch-tray-app-main/app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -18,6 +18,8 @@ package com.example.lunchtray
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.lunchtray.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -34,5 +36,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // TODO: Retrieve NavController from the NavHostFragment
+        val navHostFragment=supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController=navHostFragment.navController
+
+        //Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Handle navigation when the user Up from the action bar
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- Android Development Course Files/Unit 3/android-basics-kotlin-lunch-tray-app-main/app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ Android Development Course Files/Unit 3/android-basics-kotlin-lunch-tray-app-main/app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -71,12 +71,17 @@ class OrderViewModel : ViewModel() {
     fun setEntree(entree: String) {
         // TODO: if _entree.value is not null, set the previous entree price to the current
         //  entree price.
+        previousEntreePrice=_entree.value?.price ?: 0.0
 
         // TODO: if _subtotal.value is not null subtract the previous entree price from the current
         //  subtotal value. This ensures that we only charge for the currently selected entree.
+        _subtotal.value=(_subtotal.value ?: 0.0)-previousEntreePrice
 
         // TODO: set the current entree value to the menu item corresponding to the passed in string
+        _entree.value=menuItems[entree]
+
         // TODO: update the subtotal to reflect the price of the selected entree.
+        updateSubtotal(menuItems[entree]?.price ?: 0.0)
     }
 
     /**
@@ -84,12 +89,17 @@ class OrderViewModel : ViewModel() {
      */
     fun setSide(side: String) {
         // TODO: if _side.value is not null, set the previous side price to the current side price.
+        previousSidePrice=_side.value?.price ?: 0.0
 
         // TODO: if _subtotal.value is not null subtract the previous side price from the current
         //  subtotal value. This ensures that we only charge for the currently selected side.
+        _subtotal.value=(_subtotal.value ?: 0.0)-previousSidePrice
 
         // TODO: set the current side value to the menu item corresponding to the passed in string
+        _side.value=menuItems[side]
+
         // TODO: update the subtotal to reflect the price of the selected side.
+        updateSubtotal(menuItems[side]?.price ?: 0.0)
     }
 
     /**
@@ -98,14 +108,19 @@ class OrderViewModel : ViewModel() {
     fun setAccompaniment(accompaniment: String) {
         // TODO: if _accompaniment.value is not null, set the previous accompaniment price to the
         //  current accompaniment price.
+        previousAccompanimentPrice=_accompaniment.value?.price ?: 0.0
 
         // TODO: if _accompaniment.value is not null subtract the previous accompaniment price from
         //  the current subtotal value. This ensures that we only charge for the currently selected
         //  accompaniment.
+        _subtotal.value=(_subtotal.value ?: 0.0)-previousAccompanimentPrice
 
         // TODO: set the current accompaniment value to the menu item corresponding to the passed in
         //  string
+        _accompaniment.value=menuItems[accompaniment]
+
         // TODO: update the subtotal to reflect the price of the selected accompaniment.
+        updateSubtotal(menuItems[accompaniment]?.price ?: 0.0)
     }
 
     /**
@@ -115,8 +130,14 @@ class OrderViewModel : ViewModel() {
         // TODO: if _subtotal.value is not null, update it to reflect the price of the recently
         //  added item.
         //  Otherwise, set _subtotal.value to equal the price of the item.
+        if(_subtotal.value!=null){
+            _subtotal.value=(_subtotal.value)?.plus(itemPrice)
+        } else{
+            _subtotal.value=itemPrice
+        }
 
         // TODO: calculate the tax and resulting total
+        calculateTaxAndTotal()
     }
 
     /**
@@ -124,7 +145,10 @@ class OrderViewModel : ViewModel() {
      */
     fun calculateTaxAndTotal() {
         // TODO: set _tax.value based on the subtotal and the tax rate.
+        _tax.value=_subtotal.value?.times(taxRate)
+
         // TODO: set the total based on the subtotal and _tax.value.
+        _total.value=_subtotal.value?.plus(_tax.value ?: 0.0)
     }
 
     /**
@@ -132,5 +156,14 @@ class OrderViewModel : ViewModel() {
      */
     fun resetOrder() {
         // TODO: Reset all values associated with an order
+        previousEntreePrice=0.0
+        previousSidePrice=0.0
+        previousAccompanimentPrice=0.0
+        _entree.value=null
+        _side.value=null
+        _accompaniment.value=null
+        _subtotal.value=0.0
+        _total.value=0.0
+        _tax.value=0.0
     }
 }
