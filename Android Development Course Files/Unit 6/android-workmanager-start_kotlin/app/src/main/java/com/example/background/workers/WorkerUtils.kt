303077f140e5f0f18dc2429146290f7b93eb5ac4
namanarora09 <namanarora0908@gmail.com>
2022-08-25 21:04:08 +0530

    Android

--- Android Development Course Files/Unit 6/android-workmanager-start_kotlin/app/src/main/java/com/example/background/workers/WorkerUtils.kt
+++ Android Development Course Files/Unit 6/android-workmanager-start_kotlin/app/src/main/java/com/example/background/workers/WorkerUtils.kt
@@ -46,8 +46,6 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.util.UUID
 
-private const val TAG = "WorkerUtils"
-
 /**
  * Create a Notification that is shown as a heads-up notification if possible.
  *
@@ -57,6 +55,8 @@ private const val TAG = "WorkerUtils"
  * @param message Message shown on the notification
  * @param context Context needed to create Toast
  */
+
+private const val TAG = "WorkerUtils"
 fun makeStatusNotification(message: String, context: Context) {
 
     // Make a channel if necessary
